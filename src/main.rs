ddc6f233eb3af4a1be620527dc3c7fec1a334f94
Daniel Buch Hansen <boogiewasthere@gmail.com>
2019-12-22 11:52:42 +0100

    add log and use env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use users::get_current_uid;
+use env_logger;
+use log::*;
 use tokio::fs::OpenOptions;
 use tokio::prelude::*;
 use itertools::Itertools;
@@ -8,17 +9,26 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 
 const MIRRORLIST: &str = "/etc/pacman.d/mirrorlist";
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn check_root() {
+    let is_root = users::get_current_uid() == 0;
 
-    if get_current_uid() != 0
+    if !is_root
     {
         eprintln!("This program should be run as root!");
-        return Ok(());
+        std::process::exit(1)
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    check_root();
+
+    env_logger::init();
 
     let speculum = Speculum::new();
     let mirrors = speculum.fetch_mirrors().await?;
+
+    info!("Mirrors has been fetched!");
 
     let fetched: String =
         mirrors
@@ -35,6 +45,7 @@ async fn main() -> Result<()> {
             .write(true)
             .create(true)
             .open(MIRRORLIST).await?;
+    info!("writing mirror list!");
     mirrorlist.write(fetched.as_bytes()).await?;
 
     Ok(())
