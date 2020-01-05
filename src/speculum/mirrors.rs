fc624137ee7b02e17d95ec02a56c39af8cb2ffdf
Daniel Buch Hansen <boogiewasthere@gmail.com>
2020-01-05 17:10:52 +0100

    simplify from_utf8 usage

--- src/speculum/mirrors.rs
+++ src/speculum/mirrors.rs
@@ -4,6 +4,7 @@ use serde::Deserialize;
 use std::path::Path;
 use tokio::fs::OpenOptions;
 use tokio::prelude::*;
+use std::str::from_utf8;
 
 /// Contains information about Mirrors.
 ///
@@ -23,11 +24,11 @@ impl Mirrors {
         let mut file = tokio::fs::File::open(path).await?;
         let mut buf: Vec<u8> = Vec::new();
         file.read_to_end(&mut buf).await?;
-        Ok(serde_json::from_str(std::str::from_utf8(&buf)?)?)
+        Ok(serde_json::from_str(from_utf8(&buf)?)?)
     }
 
     pub fn load_from_buf<P: AsRef<[u8]>>(buf: P) -> Result<Mirrors> {
-        Ok(serde_json::from_str(std::str::from_utf8(buf.as_ref())?)?)
+        Ok(serde_json::from_str(from_utf8(buf.as_ref())?)?)
     }
 }
 
