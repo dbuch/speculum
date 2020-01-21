ea5cd8afb9df7e7d95c6a720d6dc95d4cacc8ad8
Daniel Buch Hansen <boogiewasthere@gmail.com>
2020-01-21 16:28:54 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ async fn main() -> Result<()> {
         .filter_protocols(options.filters.protocols)
         .order_by(|a, b| a.score.partial_cmp(&b.score).unwrap())
         .take(options.filters.latest)
-        //.save(options.optional.save)
-        .write(&mut tokio::io::stdout())
+        .save(options.optional.save)
+        //.write(&mut tokio::io::stdout())
         .await?;
 
     Ok(())
